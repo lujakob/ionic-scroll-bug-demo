677cc773ce5f3818211983131ef2234ecf203e6c
Lukas Jakob <lu.jakob@googlemail.com>
2017-01-04 11:02:18 +0100

    add demo

---
+++ src/pages/welcome/welcome.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { LoginPage } from '../login/login';
+import { SignupPage } from '../signup/signup';
+import { MainPage } from '../../pages/pages';
+
+/**
+ * The Welcome Page is a splash page that quickly describes the app,
+ * and then directs the user to create an account or log in.
+ * If you'd like to immediately put the user onto a login/signup page,
+ * we recommend not using the Welcome page.
+*/
+@Component({
+  selector: 'page-welcome',
+  templateUrl: 'welcome.html'
+})
+export class WelcomePage {
+
+  constructor(public navCtrl: NavController) {}
+
+  login() {
+    this.navCtrl.push(MainPage);
+    // this.navCtrl.push(LoginPage);
+  }
+
+  signup() {
+    // this.navCtrl.push(SignupPage);
+    this.navCtrl.push(MainPage);
+  }
+}
